762c0fadc69638fc5b51ff4b040b199e53b6a195
goncharovastacy <gggnchr@gmail.com>
2023-07-23 11:38:37 +0300

    added conditional rendering for row

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,14 +6,14 @@ function Card(props) {
     const { word, transcription, translation } = props;
     return (
         <div className={st.container}>
-            <button className={st.button_back}><img src={arrowleft} alt="" /></button>
+            <button className={st.button_back}><img src={arrowleft} alt="Arrow left" /></button>
             <div className={st.card}>
             <p className={st.word}>{word}Word</p>
-            <p className={st.transcription}>[ <span>{transcription}transcription</span> ]</p>
+            <p className={st.transcription}>{transcription}transcription</p>
             <button className={st.button}>Show translation</button>
             <p className={st.translation}>{translation}</p>
         </div>
-        <button className={st.button_forward}><img src={arrowright} alt="" /></button>
+        <button className={st.button_forward}><img src={arrowright} alt="Arrow right" /></button>
         </div>
 
     );
--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -5,14 +5,13 @@ function Row(props) {
     const { english, transcription, russian, tags, isEdit } = props;
     return ( 
         <tr>
-        <th>{isEdit ? <input type="text" placeholder={english}/> : {english}}</th>
-        <th>{isEdit ? <input type="text" placeholder={transcription}/> : {transcription}}</th>
-        <th>{isEdit ? <input type="text" placeholder={russian}/> : {russian}}</th>
-        <th>{isEdit ? <input type="text" placeholder={tags}/> : {tags}}</th>
-        <th>{isEdit ? <button>Save</button> : <button><img src={edit} alt="Edit" /></button>}</th>
-        <th>{isEdit ? <button>Cancel</button> : <button><img src={delet} alt="Delete" /></button> }</th>
+            <th>{isEdit ? <input type="text" placeholder={english}/> : english}</th>
+            <th>{isEdit ? <input type="text" placeholder={transcription}/> : transcription}</th>
+            <th>{isEdit ? <input type="text" placeholder={russian}/> : russian}</th>
+            <th>{isEdit ? <input type="text" placeholder={tags}/> : tags}</th>
+            <th>{isEdit ? <button>Save</button> : <button><img src={edit} alt="Edit" /></button>}</th>
+            <th>{isEdit ? <button>Cancel</button> : <button><img src={delet} alt="Delete" /></button> }</th>
         </tr>
      );
 }
-
 export default Row;
